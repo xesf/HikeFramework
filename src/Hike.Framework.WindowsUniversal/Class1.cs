9cab92db8ffc120b86c73404a888ec6f2f4a84dc
xesf <xesfnet@gmail.com>
2016-05-21 22:43:51 +0100

    Hike Framework using MonoGame. Added test project

---
+++ src/Hike.Framework.WindowsUniversal/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hike.Framework.WindowsUniversal
+{
+    public class Class1
+    {
+    }
+}
