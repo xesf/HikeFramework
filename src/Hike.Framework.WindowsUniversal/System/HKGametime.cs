ecaa439df6593db0765bc550acd101ffde2534be
xesf <xesfnet@gmail.com>
2016-05-22 14:54:22 +0100

    HKGameTime

---
+++ src/Hike.Framework.WindowsUniversal/System/HKGametime.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Hike.Framework.WindowsUniversal.System
+{
+    public class HKGameTime
+    {
+        public TimeSpan TotalGameTime { get; set; }
+
+        public TimeSpan ElapsedGameTime { get; set; }
+
+        public bool IsRunningSlowly { get; set; }
+
+        public HKGameTime()
+        {
+            TotalGameTime = TimeSpan.Zero;
+            ElapsedGameTime = TimeSpan.Zero;
+            IsRunningSlowly = false;
+        }
+
+        public HKGameTime(TimeSpan totalGameTime, TimeSpan elapsedGameTime)
+        {
+            TotalGameTime = totalGameTime;
+            ElapsedGameTime = elapsedGameTime;
+            IsRunningSlowly = false;
+        }
+
+        public HKGameTime(TimeSpan totalRealTime, TimeSpan elapsedRealTime, bool isRunningSlowly)
+        {
+            TotalGameTime = totalRealTime;
+            ElapsedGameTime = elapsedRealTime;
+            IsRunningSlowly = isRunningSlowly;
+        }
+    }
+}
